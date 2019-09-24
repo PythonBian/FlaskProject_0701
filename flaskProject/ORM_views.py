40d8ef66a208689f4fb48ad2ade1b6f9de334eff
laobian <laobian@qq.com>
2019-09-24 11:00:39 +0800

    flask版本更新，更新内容有： Flask ORM基本查询

--- flaskProject/ORM_views.py
+++ flaskProject/ORM_views.py
@@ -26,5 +26,61 @@ class Curriculum(models.Model):
     c_name = models.Column(models.String(32))
     c_time = models.Column(models.Date)
 
+
+
+import datetime
+
+session = models.session() #创建操作数据库的会话
+
+# c1 = Curriculum(c_id = "0002",c_name="html",c_time=datetime.datetime.now())
+# c2 = Curriculum(c_id = "0003",c_name="mysql",c_time=datetime.datetime.now())
+# c3 = Curriculum(c_id = "0004",c_name="linux",c_time=datetime.datetime.now())
+#
+# session.add_all([c1,c2,c3])
+#查所有
+# all_c = Curriculum.query.all()
+# for c in all_c:
+#     print(c.id,c.c_name)
+
+#条件查询
+# all_c = Curriculum.query.filter_by(c_id="0002") #返回列表对象
+# for c in all_c:
+#     print(c.id,c.c_name)
+#
+
+#查询一条
+# c = Curriculum.query.get(10) #返回列表对象
+# print(c)
+# print(c.id,c.c_name)
+
+# c = Curriculum.query.first()
+# print(c.id,c.c_name)
+
+#排序
+#all_c = Curriculum.query.order_by("id")
+# all_c = Curriculum.query.order_by(models.desc("id")) #倒序
+#
+# for c in all_c:
+#     print(c.id,c.c_name)
+
+# all_c = Curriculum.query.offset(2).limit(2).all() #倒序
+    #offset 偏移，在这里指的时候查询的起始位置
+    #limit 具体查询的数量
+#select * from curriculum limit 0,2 #从0号索引开始，查询两条
+# for c in all_c:
+#     print(c.id,c.c_name)
+
+#删除数据
+# c = Curriculum.query.get(4)
+# print(c)
+# session.delete(c)
+# session.commit()
+#修改数据
+c = Curriculum.query.get(3)
+c.c_name = "MySQL"
+session.add(c)
+session.commit()
+
 #创建表（同步）
-models.create_all()
+#models.create_all()
+
